84da343ae72dbacaee1e3e03bcde05c8f27f28ad
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-07-09 12:09:53 -0600

    Add LIST user endpoint

---
+++ src/router/users.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { getAllUsers } from "../controllers/users";
+import { isAutheticated } from "../middlewares";
+
+export default (router: express.Router)=> {
+    router.get('/users', isAutheticated, getAllUsers);
+};
