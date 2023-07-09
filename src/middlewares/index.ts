7c06cf3a40f8ad71c01d2aad7f6cf9b0af4e0a52
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2023-07-09 12:31:36 -0600

    Restrict DELETE endpoint to owner of the account

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -5,7 +5,7 @@ import { getUserBySessionToken } from "../db/users";
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction)=> {
     try{
         const sessionToken = req.cookies['EXPRESS-REST-API-AUTH'];
-        if(!sessionToken){ return res.sendStatus(400); }
+        if(!sessionToken){ return res.sendStatus(403); }
 
         const existingUser = await getUserBySessionToken(sessionToken);
         if(!existingUser){ return res.sendStatus(403); }
@@ -13,6 +13,21 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         merge(req, {identity: existingUser });
 
         return next();
+    }catch (error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction)=> {
+    try{
+        const { id } = req.params;
+
+        const currentUserId = get(req, 'identity._id') as string;
+        if(!currentUserId){ return res.sendStatus(403); }
+        if(currentUserId.toString() != id){ return res.sendStatus(403); }
+
+        next();
     }catch (error){
         console.log(error);
         return res.sendStatus(400);
